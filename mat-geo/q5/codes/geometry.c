047586d449f7592941253d40a3668cdbe788b482
ellantirohith <ellantirohith@gmail.com>
2024-10-06 14:47:37 +0530

    computing intersection in c

--- mat-geo/q5/codes/geometry.c
+++ mat-geo/q5/codes/geometry.c
@@ -21,35 +21,58 @@ void generate_circle(double* x_vals, double* y_vals, int num_points, double r) {
 }
 
 // Function to solve the intersection points between parabola y^2 = ax and circle x^2 + y^2 = r^2
-void solve_intersection(double* x_vals, double* y_vals, int* num_intersections, double a, double r) {
-    double b = a;
-    double c = -r * r;  // Circle equation x^2 + y^2 = r^2
+void solve_intersection(double* x_vals, double* y_vals, int* num_intersections, double a) {
+    // Parameters for the parabola y^2 = 6ax
+    double V1[2][2] = {{0, 0}, {0, 1}};
+    double u1[2] = {-3 * a, 0};
+    double f1 = 0;
     
-    // Calculate discriminant for the quadratic equation
-    double discriminant = b * b - 4 * c;
+    // Parameters for the circle x^2 + y^2 = r^2 (expressed as conic)
+    double V2[2][2] = {{ 1, 0}, {0, 1}};
+    double u2[2] = {0, 0};
+    double f2 = -16 * a * a;
+    
+    // Subtract conic parameters: V1 - V2, u1 - u2, f1 - f2
+    double V_diff[2][2] = {{V1[0][0] - V2[0][0], V1[0][1] - V2[0][1]},
+                           {V1[1][0] - V2[1][0], V1[1][1] - V2[1][1]}};
+    
+    double u_diff[2] = {u1[0] - u2[0], u1[1] - u2[1]};
+    double f_diff = f1 - f2;
+    
+    // Solving for the intersection points (quadratic equation solution)
+    // The intersection equation is: V_diff * x^2 + 2 * u_diff * x + f_diff = 0
+    // For simplicity, we assume a particular y and solve for x, or vice versa.
+
+    // Example: Solving for x when y = 0
+    double A = V_diff[0][0];  // Coefficient of x^2
+    double B = 2 * u_diff[0]; // Coefficient of x
+    double C = f_diff;        // Constant term
+    
+    // Discriminant of the quadratic equation
+    double discriminant = B * B - 4 * A * C;
     
     if (discriminant < 0) {
-        *num_intersections = 0;
+        *num_intersections = 0;  // No real solutions
         return;
     }
-
-    // Calculate both solutions for x
-    double x1 = (-b + sqrt(discriminant)) / 2;
-    double x2 = (-b - sqrt(discriminant)) / 2;
     
-    // Calculate corresponding y values using y^2 = ax
+    // Compute solutions for x
+    double x1 = (-B + sqrt(discriminant)) / (2 * A);
+    double x2 = (-B - sqrt(discriminant)) / (2 * A);
+    
+    // Corresponding y values using the parabola equation y^2 = 6ax
     x_vals[0] = x1;
-    y_vals[0] = sqrt(a * x1);
+    y_vals[0] = sqrt(6 * a * x1);
     
     x_vals[1] = x1;
-    y_vals[1] = -sqrt(a * x1);  // Negative root
+    y_vals[1] = -sqrt(6 * a * x1);  // Negative root
     
     x_vals[2] = x2;
-    y_vals[2] = sqrt(a * x2);
+    y_vals[2] = sqrt(6 * a * x2);
     
     x_vals[3] = x2;
-    y_vals[3] = -sqrt(a * x2);  // Negative root
+    y_vals[3] = -sqrt(6 * a * x2);  // Negative root
     
-    *num_intersections = 4;
+    *num_intersections = 4;  // Four intersection points
 }
 
--- mat-geo/q5/codes/plot.py
+++ mat-geo/q5/codes/plot.py
@@ -11,15 +11,14 @@ lib.generate_parabola.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTE
 lib.generate_circle.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double),
                                 ctypes.c_int, ctypes.c_double]
 lib.solve_intersection.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double),
-                                   ctypes.POINTER(ctypes.c_int), ctypes.c_double, ctypes.c_double]
+                                   ctypes.POINTER(ctypes.c_int), ctypes.c_double]
 
 # Parameters
 num_points = 1000
 x_min, x_max = 0, 20
 
 # Get the value of 'a' for the parabola equation y^2 = ax and 'r' for the circle x^2 + y^2 = r^2 from the user
-a = float(input("Enter the value of 'a' for the parabola equation y^2 = ax: "))
-r = float(input("Enter the radius 'r' for the circle equation x^2 + y^2 = r^2: "))
+a = float(input("Enter the value of 'a' for the parabola equation y^2 = 6ax: "))
 
 # Allocate memory for the x and y arrays for the parabola
 x_parabola = np.zeros(num_points, dtype=np.float64)
@@ -28,7 +27,7 @@ y_parabola = np.zeros(num_points, dtype=np.float64)
 # Call the C function to generate the parabola
 lib.generate_parabola(x_parabola.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                       y_parabola.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                      num_points, x_min, x_max, a)
+                      num_points, x_min, x_max, 6*a)
 
 # Allocate memory for the x and y arrays for the circle
 x_circle = np.zeros(num_points, dtype=np.float64)
@@ -37,7 +36,7 @@ y_circle = np.zeros(num_points, dtype=np.float64)
 # Call the C function to generate the circle
 lib.generate_circle(x_circle.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                     y_circle.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                    num_points, r)
+                    num_points, 4*a)
 
 # Allocate memory for the intersection points
 x_intersections = np.zeros(4, dtype=np.float64)
@@ -47,16 +46,16 @@ num_intersections = ctypes.c_int()
 # Call the C function to solve for intersection points with the given 'a' and 'r'
 lib.solve_intersection(x_intersections.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                        y_intersections.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                       ctypes.byref(num_intersections), a, r)
+                       ctypes.byref(num_intersections), a)
 
 # Plotting
 plt.figure()
 
 # Plot the circle
-plt.plot(x_circle, y_circle, label=f'Circle (r = {r})', color='blue')
+plt.plot(x_circle, y_circle, label=f'Circle (r = 4a)', color='blue')
 
 # Plot the parabola (both positive and negative y-values)
-plt.plot(x_parabola, y_parabola, label=f'Parabola (y^2 = {a}x)', color='orange')
+plt.plot(x_parabola, y_parabola, label=f'Parabola (y^2 = 6ax)', color='orange')
 plt.plot(x_parabola, -y_parabola, color='orange')
 
 # Highlight and label the intersection points
@@ -66,11 +65,11 @@ if num_intersections.value > 0:
     
     # Label each intersection point with custom format: (x = value a, y = 2√3 a)
     for i in range(num_intersections.value):
-        label = r"$(x = {:.0f}a, y = \pm2\sqrt{{3}}a)$".format(x_intersections[i])
+        label = r"$(x = 2a, y = \pm2\sqrt{{3}}a)$".format(x_intersections[i])
         plt.text(x_intersections[i], y_intersections[i] + 0.5, label, fontsize=12, color='black')
 
 # Determine the points that are inside the circle for the shading
-inside_circle = (x_parabola)**2 + (y_parabola)**2 <= r**2
+inside_circle = (x_parabola)**2 + (y_parabola)**2 <= (4*a)**2
 
 # Fill the region between the parabola and the circle
 plt.fill_between(x_parabola, y_parabola, 0, where=inside_circle, color='green', alpha=0.5, label='Shaded Area')
@@ -83,7 +82,7 @@ plt.gca().set_aspect('equal', adjustable='box')
 # Add labels, title, and legend
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Parabola and Circle Intersection with Labeled Points (√ symbol)")
+plt.title("Parabola and Circle Intersection with Labeled Points")
 plt.grid(True)
 plt.legend()
 
